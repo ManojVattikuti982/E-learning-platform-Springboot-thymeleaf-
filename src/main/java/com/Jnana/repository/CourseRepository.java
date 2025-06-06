fb46ce074e9eae379bfff26bc9b493027884250e
ManojVattikuti982 <manojvattikuti2000@gmail.com>
2025-06-06 13:27:12 +0530

    6. Add Course

---
+++ src/main/java/com/Jnana/repository/CourseRepository.java
@@ -0,0 +1,14 @@
+package com.Jnana.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.Jnana.model.Course;
+import com.Jnana.model.Tutor;
+
+public interface CourseRepository extends JpaRepository<Course, Long> {
+
+	List<Course> findByTutor(Tutor attribute);
+
+}
