b2bd8def4574fca7f8fb8ef512d6facb85ece83d
ManojVattikuti982 <manojvattikuti2000@gmail.com>
2025-06-08 11:06:59 +0530

    9. Add Quiz Questions for section and course

--- src/main/java/com/Jnana/service/TutorService.java
+++ src/main/java/com/Jnana/service/TutorService.java
@@ -1,9 +1,11 @@
 package com.Jnana.service;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,6 +16,7 @@ import org.springframework.web.multipart.MultipartFile;
 import com.Jnana.dto.CourseDto;
 import com.Jnana.dto.SectionDto;
 import com.Jnana.model.Course;
+import com.Jnana.model.QuizQuestion;
 import com.Jnana.model.Section;
 import com.Jnana.model.Tutor;
 import com.Jnana.repository.CourseRepository;
@@ -101,6 +104,11 @@ public class TutorService {
 				course.setPaid(courseDto.isPaid());
 				course.setDescription(courseDto.getDescription());
 				course.setTutor((Tutor) session.getAttribute("tutor"));
+				
+				List<QuizQuestion> questions = Arrays.stream(courseDto.getQuestions().split("\\?"))
+						.map(x -> new QuizQuestion(x)).collect(Collectors.toList());
+				course.setQuizQuestions(questions);
+				
 				courseRepository.save(course);
 				session.setAttribute("pass", "Course Added Success");
 				return "redirect:/tutor/courses";
@@ -180,6 +188,11 @@ public class TutorService {
 				section.setTitle(sectionDto.getTitle());
 				section.setNotesUrl(saveNotes(sectionDto.getNotes(), tutor.getName(), section.getTitle()));
 				section.setVideoUrl(saveVideo(sectionDto.getVideo(), tutor.getName(), section.getTitle()));
+				
+				List<QuizQuestion> questions = Arrays.stream(sectionDto.getQuestions().split("\\?"))
+						.map(x -> new QuizQuestion(x)).collect(Collectors.toList());
+				section.setQuizQuestions(questions);
+				
 				sectionRepository.save(section);
 				session.setAttribute("pass", "Section Added Success");
 				return "redirect:/tutor/sections";
